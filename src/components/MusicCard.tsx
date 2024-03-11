b94bd2bc464a48df7b4dbfa1f38b5bbdfdc7d79c
JyojiTenguam <j.y.oji@hotmail.com>
2024-03-11 14:56:26 -0300

    Resolvendo requisito 05 do Projeto TrybeTunes

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import { SongType } from '../types';
 
-function MusicCard({ trackName, previewUrl }: SongType) {
+function MusicCard({ trackId, trackName, previewUrl }: SongType) {
+  const [isFavorited, setFavorited] = useState(false);
+
+  const toggleFavorite = () => {
+    setFavorited(!isFavorited);
+  };
+
   return (
     <div>
-      <label>
+      <label data-testid={ `checkbox-music-${trackId}` }>
         {`${trackName}`}
+        <input
+          type="checkbox"
+          checked={ isFavorited }
+          onChange={ toggleFavorite }
+        />
+        <img
+          src={ isFavorited
+            ? '/src/images/checked_heart.png'
+            : '/src/images/empty_heart.png' }
+          alt="favorite"
+        />
         <audio data-testid="audio-component" src={ previewUrl } controls>
           <track kind="captions" />
           O seu navegador não suporta o elemento
